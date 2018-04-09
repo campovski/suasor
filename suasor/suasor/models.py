1c0a4c20e3f429f45b0c907078c48347c44dad0c
Campovski <campovski@gmail.com>
2018-04-09 16:06:04 +0200

    Hopefully it will work now...

--- suasor/suasor/models.py
+++ suasor/suasor/models.py
@@ -1,49 +1,28 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
-from __future__ import unicode_literals
-
 from django.db import models
 
 
 class Friendship(models.Model):
-    user1 = models.TextField(blank=True, null=True)
-    user2 = models.TextField(blank=True, null=True)
-
-    class Meta:
-        db_table = 'friendship'
-
-
-class Log(models.Model):
-    at_time = models.CharField(max_length=27, blank=True, null=True)
-    type = models.ForeignKey('LogType', models.DO_NOTHING, db_column='type', blank=True, null=True)
-    in_package = models.TextField(blank=True, null=True)
-    in_function = models.TextField(blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
-
-    class Meta:
-        db_table = 'log'
+    user1 = models.TextField()
+    user2 = models.TextField()
 
 
 class LogType(models.Model):
-    name = models.CharField(max_length=10, blank=True, null=True)
+    name = models.CharField(max_length=10)
 
-    class Meta:
-        db_table = 'log_type'
+
+class Log(models.Model):
+    at_time = models.CharField(max_length=27)
+    type = models.ForeignKey(LogType)
+    in_package = models.TextField()
+    in_function = models.TextField()
+    description = models.TextField()
 
 
 class Rating(models.Model):
-    user1 = models.TextField(blank=True, null=True)
-    user2 = models.TextField(blank=True, null=True)
+    user1 = models.TextField()
+    user2 = models.TextField()
     grade = models.NullBooleanField()
     trainset = models.NullBooleanField()
-
-    class Meta:
-        db_table = 'rating'
 
 
 class UserData(models.Model):
@@ -56,6 +35,3 @@ class UserData(models.Model):
     picture_url = models.TextField(blank=True, null=True)
     married = models.NullBooleanField()
     in_relationship = models.NullBooleanField()
-
-    class Meta:
-        db_table = 'user_data'

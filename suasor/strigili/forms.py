8283bfc7413e63608a2d62979d22d514f3511849
Campovski <campovski@gmail.com>
2018-04-10 12:21:31 +0200

    Add basic UI for strigili and Django view/template for it, close #2

---
+++ suasor/strigili/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class LoginForm(forms.Form):
+	user = forms.CharField(label="", help_text="", max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Username or email'}))
+	password = forms.CharField(label="", help_text="", max_length=100, widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+	depth = forms.IntegerField(label="", help_text="", min_value=1, max_value=10, widget=forms.NumberInput(attrs={'placeholder': 'Username or email'}))
+	roots = forms.CharField(label="", help_text="", max_length=200, required=False, widget=forms.TextInput(attrs={'placeholder': 'Custom roots of search'}))
--- suasor/strigili/views.py
+++ suasor/strigili/views.py
@@ -1,9 +1,32 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import threading
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from .forms import LoginForm
+from .auxilium import strigili
 
 
 def index(request):
-	return HttpResponse("Strigili ok")
+	if request.method == 'POST':
+		form = LoginForm(request.POST)
+
+		if form.is_valid():
+			username = form.cleaned_data['user']
+			password = form.cleaned_data['password']
+			depth = form.cleaned_data['depth']
+			roots = form.cleaned_data['roots']
+
+			thread = threading.Thread(target=strigili, args=[username, password, depth, roots])
+			thread.start()
+
+			return search(request)
+
+		return render(request, 'strigili/index.html', { 'form': form })
+
+	return HttpResponse("Error, bad request!")
+
+def search(request):
+	return HttpResponse("Your request is being processed... We will notify you when it's done!")

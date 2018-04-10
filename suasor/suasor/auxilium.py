cf7bd778d65a594f547e8e44e9cc1c9eaf8f2748
Campovski <campovski@gmail.com>
2018-04-10 18:11:51 +0200

    Indentation errors

--- suasor/suasor/auxilium.py
+++ suasor/suasor/auxilium.py
@@ -6,10 +6,10 @@ from .models import Log, LogType
 	@param desc: Description of error
 """
 def _log(log_type, package, at, desc):
-	log_time = datetime.datetime.now()
-	if DEBUG:
-		with open(os.path.join(DIR_DATA_LOG, '{}.log'.format(package)), 'a') as f:
-			f.write('[{0}] @ {1} @ {2}: {3}\n'.format(log_type, log_time, at, desc))
+    log_time = datetime.datetime.now()
+    if DEBUG:
+        with open(os.path.join(DIR_DATA_LOG, '{}.log'.format(package)), 'a') as f:
+            f.write('[{0}] @ {1} @ {2}: {3}\n'.format(log_type, log_time, at, desc))
 
     log = Log()
     log.at_time = log_time
